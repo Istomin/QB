16af12f7e24f1ca375c891baa74534f34516804e
COMPUTER\ivan <domnich.ivan@gmail.com>
2017-07-11 17:26:25 +0300

    Added settings model properties

--- src/app/settings-modal/settings-tabs/settings-tabs.component.ts
+++ src/app/settings-modal/settings-tabs/settings-tabs.component.ts
@@ -9,25 +9,54 @@ import { AppSettingsService } from '../../core/app-settings.service';
   templateUrl: './settings-tabs.component.html'
 })
 export class SettingsTabsComponent {
-  private settings: Settings;
+  private pageSettings: Settings;
   @ViewChild(ColorPickerComponent) colorPickerComponent: ColorPickerComponent;
 
   constructor(private settingsService: AppSettingsService) {
   }
 
   ngOnInit() {
-    this.settings = {
-      graphics: {
-        mainColors: {
-          titleBackground: '#ee0',
-          titleTextColor: '#fff'
+    this.pageSettings = {
+      settings: {
+        system: {
+          refreshInterval: 1,
+          scrollInterval: 1,
+          displayMode: 1,
+          flightDisplay: 1,
+          showTransit: true,
+          showTransitType: 1,
+          showExpectedDelivery: true,
+          dropDelivered: true
+        },
+        alerts: {
+          primaryInTransit: true,
+          primaryInTransitTime: 1,
+          primaryInTransitTextColor: '#f00',
+          primaryInTransitBackgroundColor: '#f00',
+          secondaryInTransit: true,
+          secondaryInTransitTime: 1,
+          secondaryInTransitTextColor: '#f00',
+          secondaryInTransitBackgroundColor: '#f00',
+          etaNote: true,
+          etaNoteType: 1,
+          etaNoteTextColor: '#f00',
+          etaNoteBackgroundColor: '#f00',
+        },
+        graphics: {
+          titleBackground: '#f00',
+          titleTextColor: '#f00',
+          tableHeaderColor: '#f00',
+          tableTextColor: '#f00',
+          tableRowColor1: '#f00',
+          tableRowColor2: '#f00',
+          businessName: 'Your Business Name Here'
         }
       }
-    };
+    }
   }
 
   onColorChanged(colorObj) {
-    this.settings.graphics.mainColors[colorObj.param] = colorObj.color;
+    this.pageSettings.settings.graphics[colorObj.param] = colorObj.color;
     this.settingsService.emitNavChangeEvent(colorObj);
   }
 }
