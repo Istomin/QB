7ffdc0f31e8cd68c85e4db05d8a987e860308d0c
IDOMNICH\Ivan Domnich <domnich.ivan@gmail.com>
2017-07-12 16:35:50 +0300

    Settings logic implementation

--- src/app/header-control/header-control.component.ts
+++ src/app/header-control/header-control.component.ts
@@ -1,19 +1,17 @@
-import {Component, OnInit, ViewChild, OnDestroy} from '@angular/core';
-import {SettingsModalComponent} from '.././settings-modal';
-import {AppSettingsService} from '.././core/app-settings.service';
-import {Subscription} from 'rxjs/Subscription';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
+import { SettingsModalComponent } from '.././settings-modal';
+import { AppSettingsService } from '.././core/app-settings.service';
+import { Subscription } from 'rxjs/Subscription';
 @Component({
   selector: 'header-control',
   styleUrls: [ 'header-control.component.scss' ],
   templateUrl: 'header-control.component.html'
 })
-export class HeaderControlComponent implements OnInit {
-
+export class HeaderControlComponent implements OnInit, OnDestroy {
+  @ViewChild('lgModal') public  lgModal: SettingsModalComponent;
+  public subscription: Subscription;
   private titleBackground: string;
   private titleTextColor: string;
-
-  @ViewChild('lgModal') lgModal :SettingsModalComponent;
-  subscription: Subscription;
   constructor(private settingsService: AppSettingsService) {
 
   }
@@ -21,14 +19,14 @@ export class HeaderControlComponent implements OnInit {
   public ngOnInit() {
     this.subscription = this.settingsService.getNavChangeEmitter().subscribe((response) => {
       this.onAppSettingsChanged(response);
-    })
+    });
   }
 
-  ngOnDestroy() {
+  public ngOnDestroy() {
     this.subscription.unsubscribe();
   }
 
-  onAppSettingsChanged(response:any) {
+  private onAppSettingsChanged(response: any) {
     this[response.param] = response.color;
   }
 }
--- src/app/info-table/info-table.component.ts
+++ src/app/info-table/info-table.component.ts
@@ -1,13 +1,28 @@
-import {Component, OnInit} from '@angular/core';
-
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
+import { SettingsModalComponent } from '.././settings-modal';
+import { AppSettingsService } from '.././core/app-settings.service';
+import { Subscription } from 'rxjs/Subscription';
 @Component({
   selector: 'info-table',
   styleUrls: [ 'info-table.component.scss' ],
   templateUrl: 'info-table.component.html'
 })
-export class InfoTableComponent implements OnInit {
-  constructor() {}
-  public ngOnInit() {
+export class InfoTableComponent implements OnInit, OnDestroy {
+  @ViewChild('lgModal') public  lgModal: SettingsModalComponent;
+  public subscription: Subscription;
+  constructor(private settingsService: AppSettingsService) {}
 
+  public ngOnInit() {
+    this.subscription = this.settingsService.getNavChangeEmitter().subscribe((response) => {
+      this.onAppSettingsChanged(response);
+    });
+  }
+
+  public ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  private onAppSettingsChanged(response: any) {
+    console.log(1222, response, 777);
   }
 }
