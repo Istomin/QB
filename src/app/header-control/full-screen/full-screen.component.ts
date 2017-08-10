33e87c1d30c995618fc0d26ab603d8be6fd1c846
COMPUTER\ivan <domnich.ivan@gmail.com>
2017-08-10 14:16:19 +0300

    full screen

--- src/app/header-control/full-screen/full-screen.component.ts
+++ src/app/header-control/full-screen/full-screen.component.ts
@@ -9,21 +9,30 @@ import { Component } from '@angular/core';
 })
 export class FullScreenComponent {
   public openFullScreen(): void {
-    function requestFullScreen(element: any) {
-      // Supports most browsers and their versions.
-      let requestMethod = element.requestFullScreen || element.webkitRequestFullScreen
-        || element.mozRequestFullScreen || element.msRequestFullScreen;
 
-      if (requestMethod) {
-        requestMethod.call(element);
-      } else if (window.hasOwnProperty('ActiveXObject')) {
-         //let wscript = new ActiveXObject('WScript.Shell');
-        // if (wscript !== null) {
-        //   wscript.SendKeys('{F11}');
-        // }
+    if (!document.fullscreenElement &&    // alternative standard method
+      !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {  // current working methods
+      if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+      } else if (document.documentElement.msRequestFullscreen) {
+        document.documentElement.msRequestFullscreen();
+      } else if (document.documentElement.mozRequestFullScreen) {
+        document.documentElement.mozRequestFullScreen();
+      } else if (document.documentElement.webkitRequestFullscreen) {
+        document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+      }
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
       }
     }
-    let elem = document.body; // Make the body go full screen.
-    requestFullScreen(elem);
+
+
   }
 }
