2b0fb50978b93bb7fbf1be13a4964f2bcd442ec7
Pipon <YuriV@top4.com>
2018-01-07 22:38:00 +0200

    fix scroll ossues

--- src/app/dashboard/table-scroller.directive.ts
+++ src/app/dashboard/table-scroller.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Renderer2, ElementRef } from '@angular/core';
+import { Directive, Renderer2, ElementRef, Input, EventEmitter } from '@angular/core';
 import { LocalStorageService } from '../core/local-storage.service';
 import { AppSettingsService } from '../core/app-settings.service';
 import { Subscription } from 'rxjs/Subscription';
@@ -8,54 +8,81 @@ var jQuery = require("jquery");
   selector: '[scroller]'
 })
 export class TableScrollerDirective {
+
+  @Input() scrollerApi: {
+    getScrollToOffset: (offset) => {};
+  };
+
   public subscription: Subscription;
-  private animSpeed: number = 10000;
   private prevScrollOffset: number = -1;
   private startFlag: boolean = false;
   private timer: any;
+  private lastScrollPosition: number;
+  private _componentApi: any;
 
   constructor(
     private renderer: Renderer2,
     private element: ElementRef,
     private localStorage: LocalStorageService,
     private settingsService: AppSettingsService
-  ){
+  ) {
 
   }
   ngOnInit() {
+
     this.subscription = this.settingsService.getScrollInterval().subscribe((interval) => {
       clearInterval(this.timer);
       this.startFlag = true;
-      this.animSpeed = interval;
-      this.startScrolling();
+      this.lastScrollPosition = 0;
+
+      this.scrollWithInterval();
     });
 
-    if(!this.startFlag) {
+    if (!this.startFlag) {
       setTimeout(() => {
-        this.animSpeed =  this.localStorage.getObject('userSettings').hasOwnProperty('settings') && this.localStorage.getObject('userSettings').settings.system.scrollInterval * 1000 || 10000;
-        this.startScrolling();
+
+        this.scrollWithInterval();
       }, 10000);
     }
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.subscription.unsubscribe();
   }
 
-  startScrolling() {
+  getAnimationSpeed() {
+    return this.localStorage.getObject('userSettings').hasOwnProperty('settings') && this.localStorage.getObject('userSettings').settings.system.scrollInterval * 1000 || 10000;
+  }
 
+  scrollWithInterval() {
 
-    this.timer = setInterval(() => {
-      var offset = jQuery('main').height() - 50; // get visible table part size
-      let scrollTo = jQuery(this.element.nativeElement).scrollTop() + offset;
-      if(scrollTo + offset - 1 > jQuery(this.element.nativeElement).find('tbody').height()) {
+    this.timer = setTimeout(() => {
+
+      let offset = 0;
+      let mainElementOffset = jQuery('main').height() - 50;
+
+      let element = this.element.nativeElement;
+
+      let scrollTo = 0;
+      if (this.scrollerApi && this.scrollerApi.getScrollToOffset) {
+        offset = +this.scrollerApi.getScrollToOffset(mainElementOffset);
+      }
+      scrollTo = jQuery(element).scrollTop() + offset || mainElementOffset;
+
+      if (this.lastScrollPosition === scrollTo) {
         scrollTo = 0;
       }
+
+      this.lastScrollPosition = scrollTo;
+
       jQuery(this.element.nativeElement).animate({
         scrollTop: scrollTo
       }, 500, () => {
-
+        this.scrollWithInterval();
       });
-  }, this.animSpeed);
+
+    }, this.getAnimationSpeed())
+
   }
+
 }
--- src/app/info-table/info-table.component.ts
+++ src/app/info-table/info-table.component.ts
@@ -5,20 +5,26 @@ import { AppSettingsService } from '.././core/app-settings.service';
 import { Subscription } from 'rxjs/Subscription';
 import { DomSanitizer } from '@angular/platform-browser';
 import { InfoTableService, Shipment } from '.././info-table/info-table.service';
-import {SpinnerService} from "../core/spinner/spinner.service";
+import { SpinnerService } from "../core/spinner/spinner.service";
 import { UploadService } from "../core/upload.service";
-import {LocalStorageService} from "../core/local-storage.service";
+import { LocalStorageService } from "../core/local-storage.service";
+
+import { ElementRef } from '@angular/core';
+
 
 const ETANoteType = ['WEATHER DELAY', 'NO SHOW AIRLINE', 'FLIGHT DELAY'];
 
 @Component({
   selector: 'info-table',
-  styleUrls: [ 'info-table.component.scss' ],
+  styleUrls: ['info-table.component.scss'],
   templateUrl: 'info-table.component.html',
   providers: [InfoTableService, SpinnerService, DatePipe]
 })
 export class InfoTableComponent implements OnInit, OnDestroy {
-  @ViewChild('lgModal') public  lgModal: SettingsModalComponent;
+  @ViewChild('lgModal') public lgModal: SettingsModalComponent;
+  @ViewChild('tableHeader') tableHeaderEl: ElementRef;
+  @ViewChild('tableBody') tableBodyEl: ElementRef;
+
   public subscription: Subscription;
   public tableColSubscription: Subscription;
   public refreshIntervalSubscription: Subscription;
@@ -31,7 +37,7 @@ export class InfoTableComponent implements OnInit, OnDestroy {
   private tableRowColor1: any;
   private tableRowColor2: any;
   order: string;
-  reverse : boolean = false;
+  reverse: boolean = false;
   private showOrg: boolean;
   private showShipper: boolean;
   private dropDelivered: boolean;
@@ -41,8 +47,10 @@ export class InfoTableComponent implements OnInit, OnDestroy {
   private refreshTimer: any;
   private pageSettings: any;
   private interval: number;
+  private colunmWidths: string[];
+  public scrollerApi: any;
 
-  constructor(private settingsService: AppSettingsService, private sanitizer: DomSanitizer, private infoTableService: InfoTableService, private spinner: SpinnerService, private uploadService: UploadService, private localStorage: LocalStorageService, private datePipe: DatePipe) {}
+  constructor(private settingsService: AppSettingsService, private sanitizer: DomSanitizer, private infoTableService: InfoTableService, private spinner: SpinnerService, private uploadService: UploadService, private localStorage: LocalStorageService, private datePipe: DatePipe) { }
   public ngOnInit() {
     this.subscription = this.settingsService.getTableChangeEmitter().subscribe((response) => {
       this.onAppSettingsChanged(response);
@@ -66,6 +74,41 @@ export class InfoTableComponent implements OnInit, OnDestroy {
     this.alertsSettingsSubscription = this.settingsService.getAlertsSettingsEmitter().subscribe((settings) => {
       this.applyAlertsSettings(settings);
     });
+
+    let cells = this.tableHeaderEl.nativeElement.rows[0].cells;
+
+    this.colunmWidths = [];
+    for (let i = 0; i < cells.length; i++) {
+      this.colunmWidths[i] = cells[i].clientWidth + 'px';
+    }
+
+    this.scrollerApi = {
+      getScrollToOffset: (baseOffset) => {
+
+        let rows = this.tableBodyEl.nativeElement.rows
+
+        let lastVisibleIndex = -1;
+        let visibleElementsHeight = 0;
+        for (let i = 0; i < rows.length; i++) {
+          let row = rows[i];
+          if (this.isElementInViewport(row)) {
+            lastVisibleIndex = i;
+            visibleElementsHeight += row.clientHeight;
+          }
+          else {
+            if (lastVisibleIndex !== -1 && i > lastVisibleIndex) {
+
+              if (visibleElementsHeight > baseOffset) {
+                visibleElementsHeight -= rows[lastVisibleIndex].clientHeight;
+              }
+
+              return visibleElementsHeight;
+            }
+          }
+        }
+        return baseOffset;
+      }
+    }
   }
 
   public ngOnDestroy() {
@@ -88,8 +131,8 @@ export class InfoTableComponent implements OnInit, OnDestroy {
     this.showTransit = obj.settings.system.showTransit;
     this.showExpectedDelivery = obj.settings.system.showExpectedDelivery;
 
-    if(this.shipments && this.shipments.length) {
-      if (this.dropDelivered ) {
+    if (this.shipments && this.shipments.length) {
+      if (this.dropDelivered) {
         this.shipments = this.shipmentsClone.filter((shipment) => !shipment.isDelivered);
       } else {
         this.shipments = this.deepCopy(this.shipmentsClone);
@@ -111,114 +154,114 @@ export class InfoTableComponent implements OnInit, OnDestroy {
       this.spinner.show();
       this.uploadService.getTableData()
         .subscribe(
-          (response) => {
-            this.spinner.hide();
-            this.shipments = response.TransactionResponse.Shipments.Shipment;
-            var tickers = [];
+        (response) => {
+          this.spinner.hide();
+          this.shipments = response.TransactionResponse.Shipments.Shipment;
+          var tickers = [];
 
-            this.shipments.forEach((shipment) => {
-              shipment['shipper'] = shipment['Shipper'].Address.CompanyName + "\n" + shipment['Shipper'].Address.City + ' ' + shipment['Shipper'].Address.StateProvinceCode + ' ' + shipment['Shipper'].Address.CountryCode;
-              shipment['consignee'] = shipment['Consignee'].Address.CompanyName + "\n" + shipment['Consignee'].Address.City + ' ' + shipment['Consignee'].Address.StateProvinceCode + ' ' + shipment['Consignee'].Address.CountryCode;
-              if(shipment['ShippersReference']) {
-                if(Array.isArray(shipment['ShippersReference'])) {
-                  shipment.reference = shipment['ShippersReference'].join(', ');
-                } else {
-                  shipment.reference = shipment['ShippersReference'];
-                }
+          this.shipments.forEach((shipment) => {
+            shipment['shipper'] = shipment['Shipper'].Address.CompanyName + "\n" + shipment['Shipper'].Address.City + ' ' + shipment['Shipper'].Address.StateProvinceCode + ' ' + shipment['Shipper'].Address.CountryCode;
+            shipment['consignee'] = shipment['Consignee'].Address.CompanyName + "\n" + shipment['Consignee'].Address.City + ' ' + shipment['Consignee'].Address.StateProvinceCode + ' ' + shipment['Consignee'].Address.CountryCode;
+            if (shipment['ShippersReference']) {
+              if (Array.isArray(shipment['ShippersReference'])) {
+                shipment.reference = shipment['ShippersReference'].join(', ');
               } else {
-                shipment.reference = '';
+                shipment.reference = shipment['ShippersReference'];
               }
-
-              if(shipment['Flights'] && shipment['Flights'].Flight) {
-                if(Array.isArray(shipment['Flights'].Flight)) {
-                    if (this.showOrg){
-                        var flight = shipment['Flights'].Flight[0];
-                    } else {
-                        var flight = shipment['Flights'].Flight.slice(-1)[0];
-                    }
-                    shipment['flight'] = flight.AirlineCode + flight.FlightNumber;
-                    shipment['org'] = flight.OriginAirportCode;
-                    shipment['des'] = flight.DestinationAirportCode;
-                    let arrival_date = flight.ArrivalDateTime.DateTime;
-                    let departure_date = flight.DepartureDateTime.DateTime;
-                    shipment['eta_arrival'] = arrival_date['@Year'] + '-' + arrival_date['@Month'] + '-' +  arrival_date['@Day'] + 'T' +  arrival_date['@Hour'] + ':'  +  arrival_date['@Minute'];
-                    shipment['eta_departure'] = departure_date['@Year'] + '-' + departure_date['@Month'] + '-' +  departure_date['@Day'] + 'T' +  departure_date['@Hour'] + ':'  +  departure_date['@Minute'];
-
-                } else {
-                  shipment['flight'] = shipment['Flights'].Flight.AirlineCode + shipment['Flights'].Flight.FlightNumber;
-                  shipment['org'] = shipment['Flights'].Flight.OriginAirportCode;
-                  shipment['des'] = shipment['Flights'].Flight.DestinationAirportCode;
-                  let arrival_date = shipment['Flights'].Flight.ArrivalDateTime.DateTime;
-                  let departure_date = shipment['Flights'].Flight.DepartureDateTime.DateTime;
-                  shipment['eta_arrival'] = arrival_date['@Year'] + '-' + arrival_date['@Month'] + '-' +  arrival_date['@Day'] + 'T' +  arrival_date['@Hour'] + ':'  +  arrival_date['@Minute'];
-                  shipment['eta_departure'] = departure_date['@Year'] + '-' + departure_date['@Month'] + '-' +  departure_date['@Day'] + 'T' +  departure_date['@Hour'] + ':'  +  departure_date['@Minute'];
-                }
-              } else {
-                shipment['flight'] = '';
-                shipment['org'] = '';
-                shipment['des'] = '';
-                shipment['eta_arrival'] = '';
-                shipment['eta_departure'] = '';
-              }
-              if(shipment['DeadlineDateTime']) {
-                  let deadline_time = shipment['DeadlineDateTime'].DateTime;
-                  shipment['expectedDelivery'] = deadline_time['@Year'] + '-' + deadline_time['@Month'] + '-' +  deadline_time['@Day'] + 'T' +  deadline_time['@Hour'] + ':'  +  deadline_time['@Minute'];
-              } else {
-                shipment['expectedDelivery'] = '';
-              }
-
-              shipment['isDelivered'] = shipment['ShipmentStatus'] == 'Delivered';
-              let status_time_obj = shipment['ShipmentStatusTime'].DateTime;
-              let status_time = status_time_obj['@Year'] + '-' + status_time_obj['@Month'] + '-' +  status_time_obj['@Day'] + 'T' +  status_time_obj['@Hour'] + ':'  +  status_time_obj['@Minute'];
-              status_time = this.datePipe.transform(status_time, 'MMM dd, ') + this.datePipe.transform(status_time, 'shortTime');
-              shipment.status = shipment['ShipmentStatus'] + "\n" + status_time + ' ' + shipment['ShipmentStatusLocation'];
-
-              if(shipment['ShipmentException'] && !shipment['ETADateTime']) {
-                let origin = shipment['Origin'] ? shipment['Origin'] : '';
-                tickers.push({
-                  name: shipment['ShipmentException'] + ' Shipment: ' +  shipment['ShipmentJobNumber'] + ' ' + origin
-                });
-              }
-            });
-            this.settingsService.emitRunningLineData(tickers);
-
-            this.shipmentsClone = this.deepCopy(this.shipments);
-
-            if (this.dropDelivered ) {
-              this.shipments = this.shipmentsClone.filter((shipment) => !shipment.isDelivered);
             } else {
-              this.shipments = this.deepCopy(this.shipmentsClone);
+              shipment.reference = '';
             }
 
-            if(this.localStorage.getObject('userSettings') && this.localStorage.getObject('userSettings').hasOwnProperty('settings')) {
-              this.applyAlertsSettings(this.localStorage.getObject('userSettings').settings.alerts);
+            if (shipment['Flights'] && shipment['Flights'].Flight) {
+              if (Array.isArray(shipment['Flights'].Flight)) {
+                if (this.showOrg) {
+                  var flight = shipment['Flights'].Flight[0];
+                } else {
+                  var flight = shipment['Flights'].Flight.slice(-1)[0];
+                }
+                shipment['flight'] = flight.AirlineCode + flight.FlightNumber;
+                shipment['org'] = flight.OriginAirportCode;
+                shipment['des'] = flight.DestinationAirportCode;
+                let arrival_date = flight.ArrivalDateTime.DateTime;
+                let departure_date = flight.DepartureDateTime.DateTime;
+                shipment['eta_arrival'] = arrival_date['@Year'] + '-' + arrival_date['@Month'] + '-' + arrival_date['@Day'] + 'T' + arrival_date['@Hour'] + ':' + arrival_date['@Minute'];
+                shipment['eta_departure'] = departure_date['@Year'] + '-' + departure_date['@Month'] + '-' + departure_date['@Day'] + 'T' + departure_date['@Hour'] + ':' + departure_date['@Minute'];
+
+              } else {
+                shipment['flight'] = shipment['Flights'].Flight.AirlineCode + shipment['Flights'].Flight.FlightNumber;
+                shipment['org'] = shipment['Flights'].Flight.OriginAirportCode;
+                shipment['des'] = shipment['Flights'].Flight.DestinationAirportCode;
+                let arrival_date = shipment['Flights'].Flight.ArrivalDateTime.DateTime;
+                let departure_date = shipment['Flights'].Flight.DepartureDateTime.DateTime;
+                shipment['eta_arrival'] = arrival_date['@Year'] + '-' + arrival_date['@Month'] + '-' + arrival_date['@Day'] + 'T' + arrival_date['@Hour'] + ':' + arrival_date['@Minute'];
+                shipment['eta_departure'] = departure_date['@Year'] + '-' + departure_date['@Month'] + '-' + departure_date['@Day'] + 'T' + departure_date['@Hour'] + ':' + departure_date['@Minute'];
+              }
+            } else {
+              shipment['flight'] = '';
+              shipment['org'] = '';
+              shipment['des'] = '';
+              shipment['eta_arrival'] = '';
+              shipment['eta_departure'] = '';
+            }
+            if (shipment['DeadlineDateTime']) {
+              let deadline_time = shipment['DeadlineDateTime'].DateTime;
+              shipment['expectedDelivery'] = deadline_time['@Year'] + '-' + deadline_time['@Month'] + '-' + deadline_time['@Day'] + 'T' + deadline_time['@Hour'] + ':' + deadline_time['@Minute'];
+            } else {
+              shipment['expectedDelivery'] = '';
             }
 
-            this.getTableData(null);
-          },
-          (error) =>  {
-            this.errorMessage = <any>error;
+            shipment['isDelivered'] = shipment['ShipmentStatus'] == 'Delivered';
+            let status_time_obj = shipment['ShipmentStatusTime'].DateTime;
+            let status_time = status_time_obj['@Year'] + '-' + status_time_obj['@Month'] + '-' + status_time_obj['@Day'] + 'T' + status_time_obj['@Hour'] + ':' + status_time_obj['@Minute'];
+            status_time = this.datePipe.transform(status_time, 'MMM dd, ') + this.datePipe.transform(status_time, 'shortTime');
+            shipment.status = shipment['ShipmentStatus'] + "\n" + status_time + ' ' + shipment['ShipmentStatusLocation'];
+
+            if (shipment['ShipmentException'] && !shipment['ETADateTime']) {
+              let origin = shipment['Origin'] ? shipment['Origin'] : '';
+              tickers.push({
+                name: shipment['ShipmentException'] + ' Shipment: ' + shipment['ShipmentJobNumber'] + ' ' + origin
+              });
+            }
+          });
+          this.settingsService.emitRunningLineData(tickers);
+
+          this.shipmentsClone = this.deepCopy(this.shipments);
+
+          if (this.dropDelivered) {
+            this.shipments = this.shipmentsClone.filter((shipment) => !shipment.isDelivered);
+          } else {
+            this.shipments = this.deepCopy(this.shipmentsClone);
           }
+
+          if (this.localStorage.getObject('userSettings') && this.localStorage.getObject('userSettings').hasOwnProperty('settings')) {
+            this.applyAlertsSettings(this.localStorage.getObject('userSettings').settings.alerts);
+          }
+
+          this.getTableData(null);
+        },
+        (error) => {
+          this.errorMessage = <any>error;
+        }
         );
     }, 100);
   }
 
   applyAlertsSettings(settings: any) {
-    if(this.shipments && this.shipments.length) {
+    if (this.shipments && this.shipments.length) {
 
       this.shipments.forEach((shipment) => {
-        if(shipment.hasOwnProperty('bgColor')) {
+        if (shipment.hasOwnProperty('bgColor')) {
           shipment['bgColor'] = null;
           shipment['textColor'] = null;
         }
       });
-      if(settings.secondaryInTransit) {
+      if (settings.secondaryInTransit) {
         this.shipments.forEach((shipment) => {
-          if(!shipment['isDelivered'] && shipment['InTransitTime']) {
+          if (!shipment['isDelivered'] && shipment['InTransitTime']) {
             let hour = shipment['InTransitTime'].split(' ')[0],
               min = shipment['InTransitTime'].split(' ')[2];
 
-            if(hour > +settings.secondaryInTransitTime && min > 0) {
+            if (hour > +settings.secondaryInTransitTime && min > 0) {
               shipment['bgColor'] = settings.secondaryInTransitBackgroundColor;
               shipment['textColor'] = settings.secondaryInTransitTextColor;
             }
@@ -226,13 +269,13 @@ export class InfoTableComponent implements OnInit, OnDestroy {
         });
       }
 
-      if(settings.primaryInTransit) {
+      if (settings.primaryInTransit) {
         this.shipments.forEach((shipment) => {
-          if(!shipment['isDelivered'] && shipment['InTransitTime']) {
+          if (!shipment['isDelivered'] && shipment['InTransitTime']) {
             let hour = shipment['InTransitTime'].split(' ')[0],
               min = shipment['InTransitTime'].split(' ')[2];
 
-            if(hour > +settings.primaryInTransitTime && min > 0) {
+            if (hour > +settings.primaryInTransitTime && min > 0) {
               shipment['bgColor'] = settings.primaryInTransitBackgroundColor;
               shipment['textColor'] = settings.primaryInTransitTextColor;
             }
@@ -240,9 +283,9 @@ export class InfoTableComponent implements OnInit, OnDestroy {
         });
       }
 
-      if(settings.etaNote) {
+      if (settings.etaNote) {
         this.shipments.forEach((shipment) => {
-          if(shipment['ETANote'] && shipment['ETANote'].indexOf(ETANoteType[settings.etaNoteType]) >= 0) {
+          if (shipment['ETANote'] && shipment['ETANote'].indexOf(ETANoteType[settings.etaNoteType]) >= 0) {
             shipment['bgColor'] = settings.etaNoteBackgroundColor;
             shipment['textColor'] = settings.etaNoteTextColor;
           }
@@ -250,6 +293,18 @@ export class InfoTableComponent implements OnInit, OnDestroy {
       }
 
     }
+  }
+
+  private isElementInViewport(el) {
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    );
   }
 
   private deepCopy(oldObj: any) {
